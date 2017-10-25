c7fe721e9dacad9872ad1ea9eb5390db4cbbcbe6
CSthingTemp <parker.erdmann19@auhsdschools.org>
2017-10-25 10:17:53 -0700

    Ch3 Complete
    completed

--- ChapterThree/Star.java
+++ ChapterThree/Star.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Star{
     public static void main(String[] args){
         Scanner scan=new Scanner(System.in);
-        System.out.print("How many stars?");
+        System.out.print("How many stars? ");
         final int MAXSTARS=scan.nextInt();
         System.out.println();
         
